5825ca5e5d52b475f724cca97c09a818c11570b4
Roman Imankulov <roman.imankulov@gmail.com>
2018-08-30 15:18:52 +0100

    Test with tox py3.7

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     # $ setup.py publish support.
     cmdclass={
