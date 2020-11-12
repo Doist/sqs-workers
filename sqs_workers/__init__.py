0f91239e83550460766b354566bbef6bf11c7ce4
Roman Imankulov <roman.imankulov@gmail.com>
2020-11-12 14:31:39 +0000

    Explicitly define __all__ for the top-level module

--- sqs_workers/__init__.py
+++ sqs_workers/__init__.py
@@ -1,15 +1,30 @@
-from sqs_workers.backoff_policies import (  # noqa: F401
+from sqs_workers.backoff_policies import (
     DEFAULT_BACKOFF,
     IMMEDIATE_RETURN,
     ConstantBackoff,
     ExponentialBackoff,
 )
-from sqs_workers.exceptions import SQSError  # noaq: F401
-from sqs_workers.memory_sqs import MemorySession  # noqa: F401
-from sqs_workers.queue import JobQueue, RawQueue  # noqa: F401
-from sqs_workers.sqs_env import SQSEnv  # noqa: F401
-from sqs_workers.sqs_manage import (  # noqa: F401
+from sqs_workers.exceptions import SQSError
+from sqs_workers.memory_sqs import MemorySession
+from sqs_workers.queue import JobQueue, RawQueue
+from sqs_workers.sqs_env import SQSEnv
+from sqs_workers.sqs_manage import (
     create_fifo_queue,
     create_standard_queue,
     delete_queue,
 )
+
+__all__ = [
+    "DEFAULT_BACKOFF",
+    "IMMEDIATE_RETURN",
+    "ConstantBackoff",
+    "ExponentialBackoff",
+    "SQSError",
+    "MemorySession",
+    "JobQueue",
+    "RawQueue",
+    "SQSEnv",
+    "create_fifo_queue",
+    "create_standard_queue",
+    "delete_queue",
+]
